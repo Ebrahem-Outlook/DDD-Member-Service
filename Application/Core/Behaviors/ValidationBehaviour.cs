e07a7c565a1632d28989640b9bee4b6db46d0454
Ebrahem-Outlook <EbrahemElbetar1@outlook.com>
2024-07-13 18:29:32 +0300

    V_18

---
+++ Application/Core/Behaviors/ValidationBehaviour.cs
@@ -0,0 +1,43 @@
+﻿using Application.Core.Abstractions.Messaging;
+using FluentValidation;
+using FluentValidation.Results;
+using MediatR;
+
+namespace Application.Core.Behaviors;
+
+internal sealed class ValidationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+    where TResponse : class
+{
+
+    private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+    public ValidationBehaviour(IEnumerable<IValidator<TRequest>> validators)
+    {
+        _validators = validators;
+    }
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        if (request is IQuery<TResponse>)
+        {
+            return await next();
+        }
+
+        var context = new ValidationContext<TRequest>(request);
+
+        List<ValidationFailure> failures = _validators
+            .Select(v => v.Validate(context))
+            .SelectMany(result => result.Errors)
+            .Where(f => f != null)
+            .ToList();
+
+        if (failures.Count != 0)
+        {
+            throw new ValidationException(failures);
+        }
+
+        return await next();
+    }
+}
+
